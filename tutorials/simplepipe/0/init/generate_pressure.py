da6b50b50df83f666e6c296552d8888d13e97930
Thomas <thomasdigiusto@me.com>
2019-07-24 10:44:14 +0100

    trend.py added

--- tutorials/simplepipe/0/init/generate_pressure.py
+++ tutorials/simplepipe/0/init/generate_pressure.py
@@ -7,8 +7,8 @@ with open("internalp", "w") as f:
     f.write("("+"\n")
 
     for i in range(int(nbelem/2)):
-        f.write(str(301000)+"\n")
-        f.write(str(101325)+"\n")
+        f.write(str(3)+"\n")
+        f.write(str(1)+"\n")
 
     f.write(")"+"\n")
     f.write(";"+"\n")
