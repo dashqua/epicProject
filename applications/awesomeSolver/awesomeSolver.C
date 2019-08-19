fa3e9bf14451fac341fa7a3299ea565aa4c326e3
Thomas <thomasdigiusto@me.com>
2019-08-19 16:30:31 +0100

    yes

--- applications/awesomeSolver/awesomeSolver.C
+++ applications/awesomeSolver/awesomeSolver.C
@@ -29,7 +29,7 @@ Description
 \*---------------------------------------------------------------------------*/
 
 #include "fvCFD.H"
-#include "fluidThermo.H"
+#include "psiThermo.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
@@ -46,7 +46,7 @@ int main(int argc, char *argv[])
     Info << "\nStarting time loop \n" << endl;
 
     // Choose Roe's original eigenvectors (1) or its light version (2)
-    const int method = 2;    
+    const bool Roe_eigenvectors = 0;    
     
     while (runTime.run())
       {
@@ -106,10 +106,11 @@ int main(int argc, char *argv[])
 	    const scalar contrVTilde = UTilde & normalVector;
 
 	    // Step 1b : Compute primitive differences
-	    const scalar deltaP = pRight - pLeft;
-	    const scalar deltaRho = rhoRight - rhoLeft;
-	    const vector deltaU = URight - ULeft;
-
+	    const scalar deltaP      = pRight - pLeft;
+	    const scalar deltaRho    = rhoRight - rhoLeft;
+	    const vector deltaU      = URight - ULeft;
+	    const scalar deltaContrV = (deltaU & normalVector);
+	    
 	    // Step 2 : Compute right eigenvectors
 	    // note : eigen 2 and 3 are added
 	    // note :  5-components vectors have to be separated because OF does not have that structure
@@ -119,20 +120,21 @@ int main(int argc, char *argv[])
 	    const scalar eigen1_1   = 1;
 	    const vector eigen1_234 = UTilde - aTilde*normalVector;
 	    const scalar eigen1_5   = hTilde-aTilde*contrVTilde;
-	    if (method == 1) // Roe eigenvectors
+	    if (Roe_eigenvectors) // Roe eigenvectors
 	      {  // note : NOT CODED YET (see 5th component)
 	    // second+third eigenvect is vector(0, 0, v, w, v^2 + w^2);
 	    const scalar eigen23_1   = 0;
-	    const vector eigen23_234 = deltaU - deltaContr*normalVector;
-	    const scalar eigen23_5   = (Utilde & deltaU) - contrVTilde*deltaContrV;
+	    const vector eigen23_234 = deltaU - deltaContrV*normalVector;
+	    const scalar eigen23_5   = (UTilde & deltaU) - contrVTilde*deltaContrV;
 	      }
-	    else if (method == 2) // lightRoe eigenvectors
+	    else // lightRoe eigenvectors
 	      {
 	    // second+third eigenvect is vector(0, 0, 1, 1, v + w);
 	    const scalar eigen23_1   = 0;
 	    const vector eigen23_234 = vector(1,1,1) - normalVector;
-	    const scalar eigen23_5   = (Utilde & vector(1,1,1)) - contrVTilde;
+	    const scalar eigen23_5   = (UTilde & vector(1,1,1)) - contrVTilde;
 	      }
+	    const vector eigen23_234bis = vector(1,1,1) - normalVector;
 	    // fourth eigenvect is vector(1, u, v, w, 0.5*q^2)
 	    const scalar eigen4_1   = 1;
 	    const vector eigen4_234 = UTilde;
@@ -147,7 +149,7 @@ int main(int argc, char *argv[])
 	    scalar lambda1 = mag(contrVTilde - aTilde);
 	    scalar lambda234 = mag(contrVTilde);
 	    scalar lambda5 = mag(contrVTilde + aTilde);
-	    scalar lambdaMax = max(max(lambda1,lambda2),lambda3);
+	    scalar lambdaMax = max(max(lambda1,lambda234),lambda5);
 
 	    // Step 4 : Compute wave strengths
 	    const scalar a1  = (deltaP - rhoTilde*deltaContrV)/(2*Foam::sqrt(aTilde));
@@ -177,27 +179,29 @@ int main(int argc, char *argv[])
 	    //
 	    // note : see the doc for explanation on waveTensor
 	    // note : can obviously be improved
+	    const scalar a2 = a23, a3 = a23;
 	    tensor waveTensor( 0, a2, a3,
 			      a2,  0, a3,
 			      a2, a3,  0);
-	    vector waveTensorProjection = waveTensor * normalVector;
+	    vector waveTensorProjection = waveTensor & normalVector; // (0,a2,a3) for x-sweep
 	    const scalar diffF23_1   = 0; // derived by hand	    
 	    const vector diffF23_234 = lambda234 * //waveTensor * eigen23_234;
- 	      vector(waveTensorProjection[0]*eigen23_234[0],
-		     waveTensorProjection[1]*eigen23_234[1],
-		     waveTensorProjection[2]*eigen23_234[2]);
+ 	      vector(waveTensorProjection[0] * eigen23_234bis[0],
+		     waveTensorProjection[1] * eigen23_234bis[1],    // BIS A ENLEVER
+		     waveTensorProjection[2] * eigen23_234bis[2]);
 	    const scalar diffF23_5   = lambda234 *
-	      vector(waveTensorProjection[0]*UTilde[0]*deltaU[0],
-		     waveTensorProjection[1]*UTilde[1]*deltaU[1],
-		     waveTensorProjection[2]*UTilde[2]*deltaU[2]);
+	      vector(waveTensorProjection[0] * UTilde[0] * deltaU[0],
+		     waveTensorProjection[1] * UTilde[1] * deltaU[1],
+		     waveTensorProjection[2] * UTilde[2] * deltaU[2])
+	      & vector(1,1,1);
 
-	    const scalar diff4_1   = lambda234 * a4 * eigen4_1;
-	    const vector diff4_234 = lambda234 * a4 * eigen4_234;
-	    const scalar diff4_5   = lambda234 * a4 * eigen4_5;
+	    const scalar diffF4_1   = lambda234 * a4 * eigen4_1;
+	    const vector diffF4_234 = lambda234 * a4 * eigen4_234;
+	    const scalar diffF4_5   = lambda234 * a4 * eigen4_5;
 
-	    const scalar diff5_1   = lambda5 * a5 * eigen5_1;
-	    const vector diff5_234 = lambda5 * a5 * eigen5_234;
-	    const scalar diff5_5   = lambda5 * a5 * eigen5_5;
+	    const scalar diffF5_1   = lambda5 * a5 * eigen5_1;
+	    const vector diffF5_234 = lambda5 * a5 * eigen5_234;
+	    const scalar diffF5_5   = lambda5 * a5 * eigen5_5;
 
 	    // step 5b : Assembly of the physical fluxes
 	    const scalar flux_1    = 0.5 *
@@ -208,12 +212,35 @@ int main(int argc, char *argv[])
 	      (fluxLeft5 + fluxRight5 - diffF1_5 - diffF23_5 - diffF4_5 - diffF5_5);
 
 
-	    rhoFlux[faceI]  = flux_1   * magSf;
-	    rhoUFlux[faceI] = flux_234 * magSf;
-	    EFlux[faceI]    = flux_5   * magSf;
+	    rhoFlux[faceI]  = flux_1   * magSf[faceI];
+	    rhoUFlux[faceI] = flux_234 * magSf[faceI];
+	    EFlux[faceI]    = flux_5   * magSf[faceI];
 
 	      // note : most of the calculation can be vectorized
 	  }
+
+	// Time integration
+	solve(fvm::ddt(rho)  + fvc::div(rhoFlux));
+	solve(fvm::ddt(rhoU) + fvc::div(rhoUFlux));
+	solve(fvm::ddt(E)    + fvc::div(EFlux));
+	
+	// Update Fields
+	U = rhoU / U;
+	// U.correctBoundaryConditions();
+	H = Cp/Cv*(E - 0.5*magSqr(U));
+	//H.correctBoundaryConditions();
+	dimensionedScalar CpMin = min(Cp);
+	dimensionedScalar CpMax = max(Cp);
+	dimensionedScalar hMin = CpMin*TMin;
+	dimensionedScalar hMax = CpMax*TMax;
+
+
+
+
+
+
+
+
 	
 	runTime.write();
       
